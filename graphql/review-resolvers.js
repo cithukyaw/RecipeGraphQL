6eda75ac1563e0dd94318d97b4247359beb34e1f
Sithu <cithukyaw@gmail.com>
2024-06-23 21:47:34 +0630

    Implement review resolvers

--- graphql/review-resolvers.js
+++ graphql/review-resolvers.js
@@ -1,3 +1,43 @@
 import Review from "../models/Review.js";
+import Recipe from "../models/Recipe.js";
+import Author from "../models/Author.js";
 
-export const reviewResolvers = {}
+export const reviewResolvers = {
+    Query: {
+        async review(_, {id}) {
+            return Review.findById(id);
+        },
+        async reviews(_, {limit}) {
+            return Review.find().sort({rating: -1}).limit(limit);
+        },
+    },
+    Review: {
+        recipe: async (parent, args, context, info) => {
+            const recipe = await Recipe.findById(parent.recipeId);
+            if (!recipe) {
+                throw new Error('Recipe not found');
+            }
+
+            return recipe;
+        },
+        author: async (parent, args, context, info) => {
+            const author = await Author.findById(parent.authorId);
+            if (!author) {
+                throw new Error('Author not found');
+            }
+
+            return author;
+        },
+    },
+    Mutation: {
+        async createReview(_, { input }, context) {
+            const review = new Review(input);
+            const res = await review.save();
+
+            return {
+                id: res.id,
+                ...res._doc
+            };
+        }
+    }
+}
