71f697e018d04f1f3bb07bd9cbd286cf70f39626
Sithu <cithukyaw@gmail.com>
2024-06-23 20:52:04 +0630

    Implement author resolvers

--- graphql/author-resolvers.js
+++ graphql/author-resolvers.js
@@ -1,3 +1,28 @@
 import Author from "../models/Author.js";
+import Recipe from "../models/Recipe.js";
 
-export const authorResolvers = {}
+export const authorResolvers = {
+    Query: {
+        async author(_, {id}) {
+            return Author.findById(id);
+        },
+        async authors(_, {limit}) {
+            limit = limit || 10;
+            return Author.find().sort({createdAt: -1}).limit(limit);
+        }
+    },
+    Mutation: {
+        async createAuthor(_, {input: {name}}, context) {
+            const createdAuthor = new Author({
+                name
+            });
+
+            const res = await createdAuthor.save();
+
+            return {
+                id: res.id,
+                ...res._doc
+            };
+        }
+    }
+}
